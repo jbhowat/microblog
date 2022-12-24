0309066bc02dd0d1aca93fb6aac3d285467ea9d8
James Howat <97859901+jbhowat@users.noreply.github.com>
2022-12-24 08:27:11 -0700

    added hot-toast for slick animated toast messages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,14 @@ import '../styles/globals.css';
 import React from 'react';
 import type { AppProps } from 'next/app';
 import NavBar from '../components/Navbar';
+import { Toaster } from 'react-hot-toast';
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<>
 			<NavBar />
 			<Component {...pageProps} />
+			<Toaster />
 		</>
 	);
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
-import Loader from "../components/Loader"
+import React from 'react';
+import toast from 'react-hot-toast';
 
 export default function Home() {
-  return (
-    <div>
-      <Loader show={true} />
-    </div>
-  )
+	return (
+		<div>
+			<button onClick={() => toast.success('Hello toast!')}>
+				Toast me
+			</button>
+		</div>
+	);
 }
